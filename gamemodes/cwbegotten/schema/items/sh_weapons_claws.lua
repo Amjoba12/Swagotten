5d67ce7d3aadd9a50d244b888b06de3aec6c030c
Amjoba <71528456+Amjoba12@users.noreply.github.com>
2026-04-29 21:46:36 -0400

    Create Voltist Saws weapon with custom mechanics
    Added Voltist Saws weapon with unique properties and behaviors.

--- gamemodes/cwbegotten/schema/items/sh_weapons_claws.lua
+++ gamemodes/cwbegotten/schema/items/sh_weapons_claws.lua
@@ -35,3 +35,75 @@ local ITEM = Clockwork.item:New("weapon_base");
 	ITEM.kinisgerOverride = true;
 	ITEM.onerequiredbelief = {"man_become_beast", "witch_druid"};
 ITEM:Register();
+
+
+local ITEM = Clockwork.item:New("weapon_base")
+	ITEM.name = "Voltist Saws"
+	ITEM.uniqueID = "begotten_claws_voltsaws"
+	ITEM.model = "models/quake4/w_q4_gauntlet.mdl"
+	ITEM.weight = 0.6
+	ITEM.category = "Melee"
+	ITEM.description = "Electric saws welded to their limbs by some psychopath."
+	ITEM.iconoverride = "icons/melee/sawglove.png"
+	ITEM.meleeWeapon = true
+	ITEM.isAttachment = false
+	ITEM.components = {breakdownType = "meltdown", items = {"fine_steel_chunks"}, {"steel_chunks"}} -- use "breakdown" for other type
+	ITEM.attributes = {"not_unequippable", "conditionless"}
+	ITEM.onerequiredbelief = {"the_paradox_riddle_equation"}
+    ITEM.conditionScale = 0.0001
+
+function ITEM:OnPlayerUnequipped(player, extraData)
+	if player:GetSubfaith() == "Voltism" then
+		Schema:EasyText(player, "peru", "This weapon cannot be removed, as it has become part of your flesh!")
+		return false
+	end
+
+	if self:HasPlayerEquipped(player) then
+		local useSound = self.useSound
+		
+		if !player:IsNoClipping() and (!player.GetCharmEquipped or !player:GetCharmEquipped("urn_silence")) then
+			if (useSound) then
+				if (type(useSound) == "table") then
+					player:EmitSound(useSound[math.random(1, #useSound)])
+				else
+					player:EmitSound(useSound)
+				end
+			elseif (useSound != false) then
+				player:EmitSound("begotten/items/first_aid.wav")
+			end
+		end
+		
+		if self.concealsFace == true then
+			player:SetNetVar("faceConcealed", false)
+		end
+	end
+end
+
+-- Called when a player has unequipped the item.
+function ITEM:OnTakeFromPlayer(player)
+	if (player:GetWeaponEquipped() == self) then
+		if player:Alive() and player:GetSubfaith() == "Voltism" then
+			Schema:EasyText(player, "peru", "This weapon cannot be removed, as it has become part of your flesh!")
+			return false
+		end
+
+		if self.concealsFace == true then
+			player:SetNetVar("faceConcealed", false)
+		end
+	end
+end
+
+-- Called when a player drops the item.
+function ITEM:OnDrop(player, position)
+	if (player:GetWeaponEquipped() == self) then
+		if player:Alive() and player:GetSubfaith() == "Voltism" then
+			Schema:EasyText(player, "peru", "This weapon cannot be removed, as it has become part of your flesh!")
+			return false
+		end
+	
+		Schema:EasyText(player, "firebrick", "You cannot drop an item that you are wearing.")
+		return false
+	end
+end
+
+ITEM:Register()
